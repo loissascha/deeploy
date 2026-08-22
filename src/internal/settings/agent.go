2b68e0af3642c7209b3677984936774f052b16d6
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-08-22 16:01:35 +0200

    settings

--- src/internal/settings/agent.go
+++ src/internal/settings/agent.go
@@ -1,3 +1,39 @@
 package settings
 
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
 
+type AgentSettings struct {
+	AgentID int `json:"agent_id"`
+}
+
+func LoadAgentSettings() (*AgentSettings, error) {
+	path, err := getSettingsBasePath()
+	if err != nil {
+		return nil, err
+	}
+	sPath := filepath.Join(path, "/agent.json")
+
+	c, err := os.ReadFile(sPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return createDefaultAgentSettings(), nil
+		}
+		return nil, err
+	}
+
+	var s AgentSettings
+	err = json.Unmarshal(c, &s)
+	if err != nil {
+		return nil, err
+	}
+
+	return &s, nil
+}
+
+func createDefaultAgentSettings() *AgentSettings {
+	return &AgentSettings{}
+}
--- src/internal/settings/settings.go
+++ src/internal/settings/settings.go
@@ -1,3 +1,16 @@
 package settings
 
+import (
+	"os"
+	"path/filepath"
+)
 
+func getSettingsBasePath() (string, error) {
+	confDir, err := os.UserConfigDir()
+	if err != nil {
+		return "", err
+	}
+
+	dir := filepath.Join(confDir, "/deeploy")
+	return dir, nil
+}

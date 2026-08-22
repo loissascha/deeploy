47017251dc825a4226371857196b27b3182f03f0
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-08-22 10:41:47 +0200

    server

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,5 @@
 module local/deeploy
 
 go 1.26.5
+
+require github.com/coder/websocket v1.8.15
---
+++ src/internal/server/server.go
@@ -0,0 +1,51 @@
+package server
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/coder/websocket"
+)
+
+type Server struct {
+	agents []*ServerAgent
+}
+
+type ServerAgent struct {
+	id int
+}
+
+func (s *Server) Run() error {
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := websocket.Accept(w, r, nil)
+		if err != nil {
+			panic(err)
+		}
+		defer conn.CloseNow()
+
+		ctx := r.Context()
+
+		for {
+			msgType, data, err := conn.Read(ctx)
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+
+			log.Printf("received: %s\n", data)
+
+			err = conn.Write(ctx, msgType, data)
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
+		}
+	})
+
+	log.Println("listening on :42066")
+	err := http.ListenAndServe(":42066", nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
